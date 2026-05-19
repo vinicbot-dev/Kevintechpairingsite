5d9e3b77aeab51f2f045873e7d66b42394a8820a
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2026-05-19 09:56:15 +0300

    Add files via upload

--- qr.js
+++ qr.js
@@ -8,85 +8,86 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-	default: Mbuvi_Tech,
-	useMultiFileAuthState,
-	jidNormalizedUser,
-	Browsers,
-	delay,
-	makeInMemoryStore,
+        default: Mbuvi_Tech,
+        useMultiFileAuthState,
+        jidNormalizedUser,
+        Browsers,
+        delay,
+        makeInMemoryStore,
 } = require("@whiskeysockets/baileys");
 
 function removeFile(FilePath) {
-	if (!fs.existsSync(FilePath)) return false;
-	fs.rmSync(FilePath, {
-		recursive: true,
-		force: true
-	})
+        if (!fs.existsSync(FilePath)) return false;
+        fs.rmSync(FilePath, {
+                recursive: true,
+                force: true
+        })
 };
 const {
-	readFile
+        readFile
 } = require("node:fs/promises")
 router.get('/', async (req, res) => {
-	const id = makeid();
-	async function MBUVI_MD_QR_CODE() {
-		const {
-			state,
-			saveCreds
-		} = await useMultiFileAuthState('./temp/' + id)
-		try {
-			let Qr_Code_By_Mbuvi_Tech = Mbuvi_Tech({
-				auth: state,
-				printQRInTerminal: false,
-				logger: pino({
-					level: "silent"
-				}),
-				browser: Browsers.macOS("Desktop"),
-			});
+        const id = makeid();
+        async function MBUVI_MD_QR_CODE() {
+                const {
+                        state,
+                        saveCreds
+                } = await useMultiFileAuthState('./temp/' + id)
+                try {
+                        let Qr_Code_By_Mbuvi_Tech = Mbuvi_Tech({
+                                auth: state,
+                                version: [2,3000,1033105955],
+                                printQRInTerminal: false,
+                                logger: pino({
+                                        level: "silent"
+                                }),
+                                browser: Browsers.ubuntu('Chrome'),
+                        });
 
-			Qr_Code_By_Mbuvi_Tech.ev.on('creds.update', saveCreds)
-			Qr_Code_By_Mbuvi_Tech.ev.on("connection.update", async (s) => {
-				const {
-					connection,
-					lastDisconnect,
-					qr
-				} = s;
-				if (qr) await res.end(await QRCode.toBuffer(qr));
-				if (connection == "open") {
-					await delay(5000);
-					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-					await delay(800);
-				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id, { text: 'VESPER-BOT:~' + b64data });
-	
-				   let MBUVI_MD_TEXT = `
+                        Qr_Code_By_Mbuvi_Tech.ev.on('creds.update', saveCreds)
+                        Qr_Code_By_Mbuvi_Tech.ev.on("connection.update", async (s) => {
+                                const {
+                                        connection,
+                                        lastDisconnect,
+                                        qr
+                                } = s;
+                                if (qr) await res.end(await QRCode.toBuffer(qr));
+                                if (connection == "open") {
+                                        await delay(5000);
+                                        let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                                        await delay(800);
+                                   let b64data = Buffer.from(data).toString('base64');
+                                   let session = await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id, { text: 'Ultra-X:~' + b64data });
+        
+                                   let MBUVI_MD_TEXT = `
 ╔═══════════════════
 ║『 SESSION CONNECTED』
-║ 🟢  Vesper-Xmd 
-║ 🟢  Bot
+║ 🟢  June x
+║ 🟢  supreme
 ║ 🟢  base64
 ╚═══════════════════`;
-	 await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id,{text:MBUVI_MD_TEXT},{quoted:session})
+         await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id,{text:MBUVI_MD_TEXT},{quoted:session})
 
 
 
-					await delay(100);
-					await Qr_Code_By_Mbuvi_Tech.ws.close();
-					return await removeFile("temp/" + id);
-				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
-					await delay(10000);
-					MBUVI_MD_QR_CODE();
-				}
-			});
-		} catch (err) {
-			if (!res.headersSent) {
-				await res.json({
-					code: "Service is Currently Unavailable"
-				});
-			}
-			console.log(err);
-			await removeFile("temp/" + id);
-		}
-	}
-	return await MBUVI_MD_QR_CODE()
+                                        await delay(100);
+                                        await Qr_Code_By_Mbuvi_Tech.ws.close();
+                                        return await removeFile("temp/" + id);
+                                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                                        await delay(10000);
+                                        MBUVI_MD_QR_CODE();
+                                }
+                        });
+                } catch (err) {
+                        if (!res.headersSent) {
+                                await res.json({
+                                        code: "Service is Currently Unavailable"
+                                });
+                        }
+                        console.log(err);
+                        await removeFile("temp/" + id);
+                }
+        }
+        return await MBUVI_MD_QR_CODE()
 });
 module.exports = router
--- voltah.js
+++ voltah.js
@@ -114,7 +114,7 @@ app.use('/', async (req, res, next) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Vesper-Xmd Session Server running on http://0.0.0.0:${PORT}`);
+    console.log(`JUNE-X Session Server running on http://0.0.0.0:${PORT}`);
 });
 
 module.exports = app;
