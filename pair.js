f4981a9070c04642b42c908b495a57f0d8492864
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2026-05-19 09:58:47 +0300

    Add files via upload

--- pair.js
+++ pair.js
@@ -33,13 +33,13 @@ router.get('/', async (req, res) => {
                 version: [2,3000,1033105955],
                 printQRInTerminal: false,
                 logger: pino({ level: 'fatal' }).child({ level: 'fatal' }),
-                browser: Browsers.ubuntu('Chrome'),
+                browser: ["Ubuntu", "Chrome", "20.0.04"],
             });
 
             if (!Pair_Code_By_Mbuvi_Tech.authState.creds.registered) {
                 await delay(1500);
                 num = num.replace(/[^0-9]/g, '');
-               const custom = "JUNEXBOT";
+               const custom = "JUNEULTR";
                 const code = await Pair_Code_By_Mbuvi_Tech.requestPairingCode(num,custom);
                 if (!res.headersSent) {
                     await res.send({ code });
