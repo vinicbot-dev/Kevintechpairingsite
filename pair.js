f63332b5d610a60a7ec3e8b98db8b126aad98f9c
vinicbot-dev <devkelvin903@gmail.com>
2026-05-25 12:22:24 +0300

    Add files via upload

--- pair.js
+++ pair.js
@@ -33,13 +33,13 @@ router.get('/', async (req, res) => {
                 version: [2,3000,1033105955],
                 printQRInTerminal: false,
                 logger: pino({ level: 'fatal' }).child({ level: 'fatal' }),
-                browser: ["Ubuntu", "Chrome", "20.0.04"],
+                browser: ["Windows", "Edge", "120.0"]
             });
 
             if (!Pair_Code_By_Mbuvi_Tech.authState.creds.registered) {
                 await delay(1500);
                 num = num.replace(/[^0-9]/g, '');
-               const custom = "JUNEULTR";
+               const custom = "JEXPLOIT";
                 const code = await Pair_Code_By_Mbuvi_Tech.requestPairingCode(num,custom);
                 if (!res.headersSent) {
                     await res.send({ code });
@@ -54,7 +54,7 @@ router.get('/', async (req, res) => {
                     let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                     await delay(1000);
                     let b64data = Buffer.from(data).toString('base64');
-                    let session = await Pair_Code_By_Mbuvi_Tech.sendMessage(Pair_Code_By_Mbuvi_Tech.user.id, { text: 'Ultra-X:~' + b64data });
+                    let session = await Pair_Code_By_Mbuvi_Tech.sendMessage(Pair_Code_By_Mbuvi_Tech.user.id, { text: 'VESPER-BOT:~' + b64data });
 
                     let Mbuvi_MD_TEXT = `🟢 paired successfully\n✅ session active\n Type: Base64\n`;
 
--- qr.js
+++ qr.js
@@ -41,7 +41,7 @@ router.get('/', async (req, res) => {
                                 logger: pino({
                                         level: "silent"
                                 }),
-                                browser: Browsers.ubuntu('Chrome'),
+                                browser: Browsers.ubuntu('edge'),
                         });
 
                         Qr_Code_By_Mbuvi_Tech.ev.on('creds.update', saveCreds)
@@ -57,13 +57,13 @@ router.get('/', async (req, res) => {
                                         let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                                         await delay(800);
                                    let b64data = Buffer.from(data).toString('base64');
-                                   let session = await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id, { text: 'Ultra-X:~' + b64data });
+                                   let session = await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id, { text: 'VESPER-BOT:~' + b64data });
         
                                    let MBUVI_MD_TEXT = `
 ╔═══════════════════
 ║『 SESSION CONNECTED』
-║ 🟢  June x
-║ 🟢  supreme
+║ 🟢  Vesper-Xmd 
+║ 🟢  Kevin 
 ║ 🟢  base64
 ╚═══════════════════`;
          await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id,{text:MBUVI_MD_TEXT},{quoted:session})
